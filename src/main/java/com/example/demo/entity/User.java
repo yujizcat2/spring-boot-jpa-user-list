443749649186e0280952af82e1209225b161d85f
yujizcat2 <yujizcat@gmail.com>
2025-04-04 13:48:38 +0900

    spring boot user list my sql

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,65 @@
+package com.example.demo.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import lombok.Data;
+
+/**
+ * ユーザー情報 Entity
+ */
+@Entity
+@Data
+@Table(name = "user")
+public class User {
+
+    /**
+     * ID
+     */
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /**
+     * 名前
+     */
+    @Column(name = "name")
+    private String name;
+
+    /**
+     * 住所
+     */
+    @Column(name = "address")
+    private String address;
+
+    /**
+     * 電話番号
+     */
+    @Column(name = "phone")
+    private String phone;
+
+    /**
+     * 更新日時
+     */
+    @Column(name = "update_date")
+    private Date updateDate;
+
+    /**
+     * 登録日時
+     */
+    @Column(name = "create_date")
+    private Date createDate;
+
+    /**
+     * 削除日時
+     */
+    @Column(name = "delete_date")
+    private Date deleteDate;
+}
